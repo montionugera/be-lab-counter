2b8640291c75625308ecd3914d30a21293de6824
Pasit Nusso <montionugera@gmail.com>
2022-03-20 16:52:22 +0700

    add ir logic

--- src/app.gateway.ts
+++ src/app.gateway.ts
@@ -8,6 +8,10 @@ import {
 } from '@nestjs/websockets';
 import { Logger } from '@nestjs/common';
 import { Server, Socket } from 'socket.io';
+import { Gpio } from 'onoff';
+import Timeout = NodeJS.Timeout;
+
+let gateWay: AppGateway = null;
 
 @WebSocketGateway({
     cors: {
@@ -17,6 +21,7 @@ import { Server, Socket } from 'socket.io';
 export class AppGateway
     implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
 {
+    ticker: Timeout;
     @WebSocketServer() server: Server;
     private logger: Logger = new Logger('AppGateway');
 
@@ -27,6 +32,7 @@ export class AppGateway
 
     afterInit(server: Server) {
         this.logger.log('Init');
+        gateWay = this;
     }
 
     handleDisconnect(client: Socket) {
@@ -34,15 +40,42 @@ export class AppGateway
     }
 
     handleConnection(client: Socket, ...args: any[]) {
+        const gateWay = this;
         this.logger.log(`Client connected: ${client.id}`);
         const [logger, server] = [this.logger, this.server];
         function loopTicker() {
+            gateWay.ticker && clearTimeout(gateWay.ticker);
             logger.log(`ticker :`, client.id);
-            server.emit('message', 'BLA BLA 🤗');
-            setTimeout(() => {
+            server.emit('message', 'pass');
+            gateWay.ticker = setTimeout(() => {
                 loopTicker();
-            }, 1500);
+            }, 3000);
         }
         loopTicker();
     }
+
+    handleIrValueChange(value: number) {
+        this.server.emit('message', `ir-${value}`);
+    }
+}
+try {
+    const LED = new Gpio(2, 'out');
+    const irSensor = new Gpio(23, 'in', 'both');
+    irSensor.watch(function (err, value) {
+        //Watch for hardware interrupts on pushButton GPIO, specify callback function
+        if (err) {
+            //if an error
+            console.error('There was an error', err); //output error message to console
+            return;
+        }
+        console.log(value);
+        if (++value == 1) {
+            gateWay && gateWay.handleIrValueChange(++value);
+            LED.writeSync(1);
+        } else {
+            LED.writeSync(0);
+        }
+    });
+} catch (e) {
+    console.log('fail to init gpio', e);
 }
